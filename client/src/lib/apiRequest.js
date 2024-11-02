6477d0f4b965784bdae2402f836041ee2c111da6
Farzan Irani <133148809+Farzanirani@users.noreply.github.com>
2024-11-02 17:51:46 +0530

    Update apiRequest.js changed to render url replacing localhost url

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8800/api",
+    baseURL: "https://estate-vtw4.onrender.com/api",
     withCredentials:true,
 })
 
